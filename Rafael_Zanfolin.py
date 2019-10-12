c81c1fdea5adee4fc68cfb28ac6d8c0c85403055
RafaelHBZ <rafaelrhbz@gmail.com>
2019-10-12 13:00:34 -0300

    Aqui o Rafael começou uma feature

--- Rafael_Zanfolin.py
+++ Rafael_Zanfolin.py
@@ -5,3 +5,22 @@ Created on Sat Oct 12 12:34:40 2019
 @author: RafaelzZ
 """
 
+jogo_10começa = 0
+
+dinheiro = int(input("Quanto dinheiro você quer apostar"))
+
+if dinheiro < 0:
+    print("Você está maluco?!?")
+elif dinheiro == 0:
+    print("Vai se fude")
+else:
+    jogo_começa = True
+    
+if jogo_começa == True:
+    print("O jogo vai começar!")
+    
+while jogo_começa == True:
+    if dinheiro > 0:
+        dinheiro = 0
+    elif dinheiro == 0:
+        jogo_começa = False
