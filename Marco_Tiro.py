1fa3a7d757208865fbbe1771818ad39d7ddca71a
RafaelHBZ <rafaelrhbz@gmail.com>
2019-10-12 12:36:09 -0300

    Aqui foi iniciado o trabalho

---
+++ Marco_Tiro.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 12:30:37 2019
+
+@author: RafaelzZ
+"""
+
---
+++ Versão_Final.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 12:34:59 2019
+
+@author: RafaelzZ
+"""
+
