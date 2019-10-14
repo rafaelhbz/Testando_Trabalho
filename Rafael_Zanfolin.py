0a68f717d5ad63b448cbcf0d11490ec9b6a604ce
RafaelHBZ <rafaelrhbz@gmail.com>
2019-10-14 11:23:12 -0300

    Aqui foi criado o começo do jogo. Faltam adicionar os features e a pontuação

--- Rafael_Zanfolin.py
+++ Rafael_Zanfolin.py
@@ -11,8 +11,7 @@ import math
 baralho_jogado = {}
 
 valor = 0
-    
-sorteio = str(random.choice(chaves))
+
 #if baralho_jogado[sorteio] == 0:
     #del baralho_jogado[sorteio]
     #print("Deu ruim")
@@ -48,69 +47,165 @@ def retirar_carta():
     # Aqui é iniciada a primera rodada
     primeira_rodada = 0
     while primeira_rodada < 2:   
-        sorteio = str(random.choice(chaves))
+        sorteio_local = str(random.choice(list(baralho.keys())))
         
-        if sorteio == "Ás":
-            numero_As == numero_As - 1
-        elif sorteio == "Dois":
+        if sorteio_local == "Ás":
+            if numero_As == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dois":
+            if numero_Dois == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Três":
+            if numero_Tres == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Quatro":
+            if numero_Quatro == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Cinco":
+            if numero_Cinco == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Seis":
+            if numero_Seis == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Sete":
+            if numero_Sete == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Oito":
+            if numero_Oito == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Nove":
+            if numero_Nove == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dez":
+            if numero_Dez == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Valete":
+            if numero_Valete == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dama":
+            if numero_Dama == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Rei":
+            if numero_Rei == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        
+        if sorteio_local == "Ás":
+            numero_As = numero_As - 1
+        if sorteio_local == "Dois":
             numero_Dois = numero_Dois - 1
-        elif sorteio == "Três":
+        if sorteio_local == "Três":
             numero_Tres = numero_Tres - 1
-        elif sorteio == "Quatro":
+        if sorteio_local == "Quatro":
             numero_Quatro = numero_Quatro - 1
-        elif sorteio == "Cinco":
+        if sorteio_local == "Cinco":
             numero_Cinco = numero_Cinco - 1
-        elif sorteio == "Seis":
+        if sorteio_local == "Seis":
             numero_Seis = numero_Seis - 1
-        elif sorteio == "Sete":
+        if sorteio_local == "Sete":
             numero_Sete = numero_Sete - 1
-        elif sorteio == "Oito":
+        if sorteio_local == "Oito":
             numero_Oito = numero_Oito - 1
-        elif sorteio == "Nove":
+        if sorteio_local == "Nove":
             numero_Nove = numero_Nove - 1
-        elif sorteio == "Dez":
+        if sorteio_local == "Dez":
             numero_Dez = numero_Dez - 1
-        elif sorteio == "Valete":
+        if sorteio_local == "Valete":
             numero_Valete = numero_Valete - 1
-        elif sorteio == "Dama":
+        if sorteio_local == "Dama":
             numero_Dama = numero_Dama - 1
-        elif sorteio == "Rei":
+        if sorteio_local == "Rei":
             numero_Rei = numero_Rei - 1
             
         primeira_rodada = primeira_rodada + 1
     
-    sorteio_local = str(random.choice(chaves))
     deseja_retirar = str(input("Você quer retirar mais uma carta? "))
     
     while deseja_retirar == "Sim" or deseja_retirar == "sim":
-        if numero_As == 0:
-            del chaves[0]
-        if numero_Dois == 0:
-            del chaves[1]
-        if numero_Tres == 0:
-            del chaves[2]
-        if numero_Quatro == 0:
-            del chaves[3]
-        if numero_Cinco == 0:
-            del chaves[4]
-        if numero_Seis == 0:
-            del chaves[5]
-        if numero_Sete == 0:
-            del chaves[6]
-        if numero_Oito == 0:
-            del chaves[7]
-        if numero_Nove == 0:
-            del chaves[8]
-        if numero_Dez == 0:
-            del chaves[9]
-        if numero_Valete == 0:
-            del chaves[10]
-        if numero_Dama == 0:
-            del chaves[11]
-        if numero_Rei == 0:
-            del chaves[12]
+        deseja_retirar = str(input("Você quer retirar mais uma carta? "))
+        lista_numeros = [numero_As, numero_Dois, numero_Tres, numero_Quatro, numero_Cinco, numero_Seis, numero_Sete, numero_Oito, numero_Nove, numero_Dez, numero_Valete, numero_Dama, numero_Rei]
+        sorteio_local = str(random.choice(list(baralho.keys())))
             
-        sorteio_local
+        if sorteio_local == "Ás":
+            if numero_As == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dois":
+            if numero_Dois == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Três":
+            if numero_Tres == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Quatro":
+            if numero_Quatro == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Cinco":
+            if numero_Cinco == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Seis":
+            if numero_Seis == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Sete":
+            if numero_Sete == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Oito":
+            if numero_Oito == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Nove":
+            if numero_Nove == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dez":
+            if numero_Dez == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Valete":
+            if numero_Valete == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Dama":
+            if numero_Dama == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+        if sorteio_local == "Rei":
+            if numero_Rei == 0:
+                sorteio_local = str(random.choice(list(baralho.keys())))
+            
+        if numero_As == 0:
+            if baralho["Ás"] in baralho:
+                del baralho["Ás"]
+        if numero_Dois == 0:
+            if baralho["Dois"] in baralho:
+                del baralho["Dois"]
+        if numero_Tres == 0:
+            if baralho["Três"] in baralho:
+                del baralho["Três"]
+        if numero_Quatro == 0:
+            if baralho["Quatro"] in baralho:
+                del baralho["Quatro"]
+        if numero_Cinco == 0:
+            if baralho["Cinco"] in baralho:
+                del baralho["Cinco"]
+        if numero_Seis == 0:
+           if baralho["Seis"] in baralho:
+                del baralho["Seis"]
+        if numero_Sete == 0:
+            if baralho["Sete"] in baralho:
+                del baralho["Sete"]
+        if numero_Oito == 0:
+            if baralho["Oito"] in baralho:
+                del baralho["Oito"]
+        if numero_Nove == 0:
+            if baralho["Nove"] in baralho:
+                del baralho["Nove"]
+        if numero_Dez == 0:
+            if baralho["Dez"] in baralho:
+                del baralho["Dez"]
+        if numero_Valete == 0:
+            if baralho["Valete"] in baralho:
+                del baralho["Valete"]
+        if numero_Dama == 0:
+            if baralho["Dama"] in baralho:
+                del baralho["Dama"]
+        if numero_Rei == 0:
+            if baralho["Rei"] in baralho:
+                del baralho["Rei"]
+                
+            
         if numero_As > 0:
             if sorteio_local == "Ás":
                 numero_As = numero_As - 1
@@ -150,14 +245,14 @@ def retirar_carta():
         if numero_Rei > 0:
             if sorteio_local == "Rei":
                 numero_Rei = numero_Rei - 1
-        deseja_retirar = str(input("Você quer retirar mais uma carta? "))
         
+        print(baralho)
+        print(lista_numeros)
     else:
         print("Ok, você decidiu parar aqui. Então vamos analisar seus pontos!")
         
     lista_numeros = [numero_As, numero_Dois, numero_Tres, numero_Quatro, numero_Cinco, numero_Seis, numero_Sete, numero_Oito, numero_Nove, numero_Dez, numero_Valete, numero_Dama, numero_Rei]
     print(lista_numeros)
-    print(chaves)
     return lista_numeros
 
 retirar_carta()        
